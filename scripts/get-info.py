ec095d606b4a97f170d8cf5cc3210dc17f5777f9
Petter Dahl Thunaes <petterthunaes@gmail.com>
2016-11-03 08:55:11 +0100

    added scrap script

---
+++ scripts/get-info.py
@@ -0,0 +1,68 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+import config
+
+# Parameters
+
+URL_LOGIN=isUrlLogin
+URL_OVERVIEW=isUrlOverivew
+URL_MYPAGE=isUrlMyPage
+
+username=isUsername
+password=isPassword
+
+headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36"}
+
+s=requests.Session()
+s.headers.update(headers)
+r=s.get(URL_LOGIN)
+
+thai=BeautifulSoup(r.content, "html5lib")
+
+VIEWSTATE=thai.find(id="__VIEWSTATE")['value']
+EVENTVALIDATION=thai.find(id="__EVENTVALIDATION")['value']
+
+login_data={"__VIEWSTATE":VIEWSTATE,
+"__EVENTVALIDATION":EVENTVALIDATION,
+"ctl00$ContentPlaceHolder1$tbUserID":username,
+"ctl00$ContentPlaceHolder1$tbPIN":password,
+"ctl00$ContentPlaceHolder1$btnHiddenSubmit":""}
+
+r=s.post(URL_LOGIN, data=login_data)
+
+# Find bandwidth
+
+k=s.get(URL_OVERVIEW)
+
+ka_data = []
+
+ka=BeautifulSoup(k.content, "html5lib")
+
+ka_data.append(ka.find(id="ctl00_ContentPlaceHolder1_hfUsageLimit")['value'])
+ka_data.append(ka.find(id="ctl00_ContentPlaceHolder1_hfUsageUsed")['value'])
+
+# Find information
+
+f=s.get(URL_MYPAGE)
+
+fa_out = []
+fa_data = []
+
+fa=BeautifulSoup(f.content, "html5lib")
+
+fa_table = fa.find('table', id='tblAbonnement')
+fa_rows = fa_table.findAll('tr')
+
+for tr in fa_rows:
+  fa_cols = tr.findAll('td')
+  for td in fa_cols:
+    fa_text = td.get_text(' ', strip=True)
+    fa_out.append(fa_text)
+
+fa_data.append(fa_out[3]);
+fa_data.append(fa_out[4]);
+
+obj = {"bandwidth": {"limit": ka_data[0], "usage": ka_data[1]}, "info": {"sim": fa_data[0], "ip": fa_data[1]}}
+
+print(json.dumps(obj))
