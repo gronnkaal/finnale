2b719f0f8cb5730ece605f8e569ce98039d2fad9
Petter Dahl Thunaes <petterthunaes@gmail.com>
2016-11-20 00:01:26 +0100

    added lean

--- routes/ice.js
+++ routes/ice.js
@@ -11,13 +11,13 @@ var Ice 	= require('../schema/ice.js');
 module.exports = function () {
 
   router.get('/', isAuthenticated, function(req, res, next) {
-    Ice.find().sort({_id:-1}).limit(1).exec(function(err,obj) {
-        res.json({
-          ip: obj[0].toObject().ip,
-          usage: obj[0].toObject().usage,
-          limit: obj[0].toObject().limit,
-          created: obj[0].toObject().created_at,
-        });
+    Ice.find().sort({_id:-1}).limit(1).lean().exec(function(err,obj) {
+      res.json({
+        ip: obj[0].ip,
+        usage: obj[0].usage,
+        limit: obj[0].limit,
+        created: obj[0].created_at,
+      });
     });
   });
 
