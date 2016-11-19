e9eb4123af9058b579bfec3b06331a5a62c21f22
Petter Dahl Thunaes <petterthunaes@gmail.com>
2016-11-20 00:05:41 +0100

    fixed error

--- routes/ice.js
+++ routes/ice.js
@@ -12,12 +12,22 @@ module.exports = function () {
 
   router.get('/', isAuthenticated, function(req, res, next) {
     Ice.find().sort({_id:-1}).limit(1).lean().exec(function(err,obj) {
-      res.json({
-        ip: obj[0].ip,
-        usage: obj[0].usage,
-        limit: obj[0].limit,
-        created: obj[0].created_at,
-      });
+      if (err) {
+        console.log('Error: No data found..');
+        res.json({
+          ip: 'xx.xx.xx.xx',
+          usage: 'xxxx',
+          limit: 'yyyy',
+          created: 'xx-xx-xx xx:xx',
+        });
+      } else {
+        res.json({
+          ip: obj[0].ip,
+          usage: obj[0].usage,
+          limit: obj[0].limit,
+          created: obj[0].created_at,
+        });
+      }
     });
   });
 
